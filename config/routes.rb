f2f0615d6b4a0f15d116be1c15797e9dbb85ced1
Ashlea Tate <ashleatate@Ashleas-Air.att.net>
2015-04-19 15:28:54 -0700

    Sunday commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => redirect('/ideas')
   resources :ideas
   resources :adventures
   # The priority is based upon order of creation: first created -> highest priority.
